ed1cced8e71ab89e8a7f66a14ee33c23cbce8165
Sudha Sree Yerramsetty <90265383+i-code-too@users.noreply.github.com>
2026-06-11 14:00:31 -0500

    Update Word embeddings (NLP 1/4)

--- model/embeddings.py
+++ model/embeddings.py
@@ -8,4 +8,5 @@ class Solution:
         # token_ids: 1D array of integer token IDs
         # Return the embedding vectors for the given token IDs
         # return np.round(your_answer, 5)
-        return np.round(embeddings[token_ids], 5)
+        output = embeddings[token_ids]
+        return np.round(output, 5)
